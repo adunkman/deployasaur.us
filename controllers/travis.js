29a6454197833cfc418a899b571755618c42fb12
Andrew Dunkman <andrew@dunkman.org>
2012-11-20 07:59:50 -0600

    Tried reading travis config from github

--- controllers/travis.js
+++ controllers/travis.js
@@ -1,30 +1,51 @@
-var travis = module.exports = {};
-
-var pending = [];
-
-travis.notify = function (req, res, next) {
-  if (pending.length) {
-    pending[0].next();
-
-    for (var i = pending.length - 1; i >= 1; i--) {
-      pending[i].res.send("");
-    };
-  }
-
-  pending = [];
-  next();
-};
+var request = require("request"),
+    travis = module.exports = {},
+    pending = {};
 
 travis.deploy = function (req, res, next) {
-  res.locals.app = "adunkman";
-  res.locals.key = "818193c0a8edd289d284a155b0e112ca";
-  res.locals.branch = "master";
+  var commit = req.params.commit,
+      repo = "adunkman/dunkman.org",
+      app = "adunkman",
+      key = "818193c0a8edd289d284a155b0e112ca",
+      branch = "master";
+
+  res.locals.app = app;
+  res.locals.key = key;
+  res.locals.branch = branch;
 
   res.writeHead(200);
 
-  pending.push({
+  pending[commit].push({
     res: res,
     next: next
+  });
+
+  getBuildMatrix(repo, commit, function (err, matrix) {
+    if (err) return next(err);
+
+    if (matrix.length === pending[commit].length) {
+      pending[commit][0].next();
+
+      for (var i = pending[commit].length - 1; i >= 1; i--) {
+        pending[commit][i].res.end();
+      };
+
+      delete pending[commit];
+    }
+  });
+};
+
+var getBuildMatrix = function (repo, commit, callback) {
+  var yml = "https://raw.github.com/" + repo + "/" + commit + "/.travis.yml";
+
+  request.get(yml, function (err, res, body) {
+    if (err) return callback(err);
+    if (res.statusCode !== 200) return callback(res.statusCode);
+
+    // TODO: Parse YAML body and determine all build configurations that
+    // travis will run.
+
+    return callback(["0.8", "0.6"]);
   });
 };
 
