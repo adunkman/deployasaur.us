bdd9e6f05b128e14927cd71bff8e1897b210b476
Andrew Dunkman <andrew@dunkman.org>
2012-11-19 21:59:21 -0600

    Added delayed responses

--- controllers/travis.js
+++ controllers/travis.js
@@ -1,6 +1,17 @@
 var travis = module.exports = {};
 
+var pending = [];
+
 travis.notify = function (req, res, next) {
+  if (pending.length) {
+    pending[0].next();
+
+    for (var i = pending.length - 1; i >= 1; i--) {
+      pending[i].res.send(200);
+    };
+  }
+
+  pending = [];
   next();
 };
 
@@ -9,5 +20,8 @@ travis.deploy = function (req, res, next) {
   res.locals.key = "818193c0a8edd289d284a155b0e112ca";
   res.locals.branch = "master";
 
-  next();
+  pending.push({
+    res: res,
+    next: next
+  });
 };
