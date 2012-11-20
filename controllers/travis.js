9fd37c4383c460b16fdd22a345f607ad000abcda
Andrew Dunkman <andrew@dunkman.org>
2012-11-19 22:36:20 -0600

    Added keepalive to responses to prevent H12

--- controllers/travis.js
+++ controllers/travis.js
@@ -20,8 +20,20 @@ travis.deploy = function (req, res, next) {
   res.locals.key = "818193c0a8edd289d284a155b0e112ca";
   res.locals.branch = "master";
 
+  res.writeHead(200);
+
   pending.push({
     res: res,
     next: next
   });
 };
+
+var keepalive = function () {
+  for (var i = pending.length - 1; i >= 0; i--) {
+    pending[i].res.write(" ");
+  };
+
+  setTimeout(keepalive, 20000);
+};
+
+keepalive();
