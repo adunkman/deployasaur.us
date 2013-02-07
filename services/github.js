5d6e216663075e9729ba735fcff90e69405ad5e0
Andrew Dunkman <andrew@dunkman.org>
2013-02-06 22:16:55 -0600

    Added organization repositories to accessable repos list, fixes #1

--- services/github.js
+++ services/github.js
@@ -1,4 +1,5 @@
 var request = require("request"),
+    async = require("async"),
     User = require("../models/user"),
     Repo = require("../models/repo"),
     github = module.exports = function (req, res, next) {
@@ -38,7 +39,64 @@ GithubService.prototype.getUser = function (callback) {
 };
 
 GithubService.prototype.getRepos = function (callback) {
+  var url = "https://api.github.com/user/orgs",
+      qs = { access_token: this.session.oauth && this.session.oauth.accessToken };
+
+  async.parallel({
+    userRepos: function (done) { this.getUserRepos(done); }.bind(this),
+    orgRepos: function (done) {
+      request.get(url, { qs: qs }, function (err, res, body) {
+        if (err) return callback("Github: unable to fetch orgs: " + err.toString());
+        if (res.statusCode !== 200) return callback("Github: unexpected status code when fetching orgs: " + res.statusCode);
+
+        try { body = JSON.parse(body); }
+        catch (e) { return callback("Github: unable to parse orgs response: " + e.toString()); }
+
+        var tasks = [];
+
+        for (var i = body.length - 1; i >= 0; i--) {
+          (function (orgName) {
+            tasks.push(function (completed) { 
+              this.getOrgRepos(orgName, completed); 
+            }.bind(this));
+          }.bind(this))(body[i].login);
+        };
+
+        async.parallel(tasks, function (err, results) {
+          if (err) return callback(err);
+          done(null, [].concat.apply([], results));
+        });
+      }.bind(this));
+    }.bind(this)
+  }, function (err, results) {
+    if (err) return callback(err);
+    callback(null, results.userRepos.concat(results.orgRepos));
+  });
+};
+
+GithubService.prototype.getUserRepos = function (callback) {
   var url = "https://api.github.com/user/repos",
+      qs = { access_token: this.session.oauth && this.session.oauth.accessToken };
+
+  request.get(url, { qs: qs }, function (err, res, body) {
+    if (err) return callback("Github: unable to fetch repositories: " + err.toString());
+    if (res.statusCode !== 200) return callback("Github: unexpected status code when fetching repositories: " + res.statusCode);
+
+    try { body = JSON.parse(body); }
+    catch (e) { return callback("Github: unable to parse repos response: " + e.toString()); }
+
+    var repos = [];
+
+    for (var i = body.length - 1; i >= 0; i--) {
+      repos.push(new Repo(body[i]));
+    }
+
+    return callback(null, repos);
+  });
+};
+
+GithubService.prototype.getOrgRepos = function (org, callback) {
+  var url = "https://api.github.com/orgs/" + org + "/repos",
       qs = { access_token: this.session.oauth && this.session.oauth.accessToken };
 
   request.get(url, { qs: qs }, function (err, res, body) {
